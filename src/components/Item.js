c57057e9ddaa3715b725ea9c4bcccb272448bb35
root <gloria.kitili@student.moringaschool.com>
2023-01-21 10:44:16 +0300

    completed state-events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
-function Item({ name, category }) {
+function Item({name,category}){
+  const[inCart,setClassName]=useState(false)
+
+  const AddedToCart =() =>{
+    setClassName(()=> !inCart)
+  }
+  return(
+    <li className = {AddedToCart ? 'in-Cart':''}>
+      <span>{name}</span>
+    <span className = 'category'> {category} </span>
+    <button className = 'add' onClick ={AddedToCart}> 
+    {AddedToCart ? 'Add to' : 'Remove From'}
+    </button>
+    </li>
+  )
+}
+
+/*function Item({ name, category }) {
   return (
     <li className="">
       <span>{name}</span>
@@ -8,6 +25,6 @@ function Item({ name, category }) {
       <button className="add">Add to Cart</button>
     </li>
   );
-}
+}*/
 
 export default Item;
